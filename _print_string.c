3a35d8749c33045029d0d92f2fe689460f447ad3
osinnworld <shemogata@gmail.com>
2023-08-24 19:37:09 -0700

    5:36 am changes

--- _print_string.c
+++ _print_string.c
@@ -8,9 +8,31 @@
 
 int print_string(va_list ls)
 {
-	char *strg = va_arg(ls, char*);
-	int i;
+	char *p;
+	int p_len = 0;
+	int str_len;
+	char *fmt_str;
 
-	i = print((strg != NULL) ? strg : "(null)");
-	return (i);
+	p = va_arg(ls, char*);
+
+	if (p == NULL)
+	{
+		p = "(null)";
+	}
+
+	str_len = _strlen(p);
+
+	fmt_str = malloc(str_len + 1);
+
+	if (fmt_str == NULL)
+	{
+		return (0);
+	}
+
+	strcpy(fmt_str, p);
+	p_len = print(fmt_str);
+
+	free(fmt_str);
+
+	return (p_len);
 }
--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 int _printf(const char *format, ...);
 
