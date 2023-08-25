2887c81e2c88773b3c338474419d3eb1556391e8
osinnworld <shemogata@gmail.com>
2023-08-24 20:07:24 -0700

    changed to handle \x

--- _print_string.c
+++ _print_string.c
@@ -5,34 +5,31 @@
  *
  * Return: length
  */
-
 int print_string(va_list ls)
 {
 	char *p;
 	int p_len = 0;
-	int str_len;
-	char *fmt_str;
-
+	char fmt_str[5];
+	
 	p = va_arg(ls, char*);
-
+	
 	if (p == NULL)
 	{
 		p = "(null)";
 	}
 
-	str_len = _strlen(p);
-
-	fmt_str = malloc(str_len + 1);
-
-	if (fmt_str == NULL)
+	for (; *p != '\0'; p++)
 	{
-		return (0);
+		if (*p < 32 || *p >= 127)
+		{
+			snprintf(fmt_str, sizeof(fmt_str), "\\x%02X", *p);
+			p_len += print(fmt_str);
+		}
+		else
+		{
+			p_len += _putchar(*p);
+		}
 	}
-
-	strcpy(fmt_str, p);
-	p_len = print(fmt_str);
-
-	free(fmt_str);
 
 	return (p_len);
 }
