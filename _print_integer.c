50fe34c30982ffad869b2f4e108a466f4431f520
osinnworld <shemogata@gmail.com>
2023-08-24 20:22:37 -0700

    modify integer to handle + #

--- _print_integer.c
+++ _print_integer.c
@@ -10,11 +10,18 @@ int print_integer(va_list ls)
 {
 	char *pt;
 	int sz;
+
 	int num = va_arg(ls, int);
+	char sign = (value < 0) ? '-' : '+';
 
 	pt = int_asc(num, 10);
 
-	sz = print((pt != NULL) ? pt : "NULL");
+	if
+	{
+		sz = _putchar(sign);
+	}
+
+	sz += print((pt != NULL) ? pt : "NULL");
 
 	return (sz);
 }
